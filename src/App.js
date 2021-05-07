9d7b21f0db805bb4c7eeef5cdb25afec5caa95e7
Adam Horscraft <adam.horscraft@foodhub.co.uk>
2021-05-07 11:42:05 +0100

    updated components

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      newJoke: '',
+      joke: {},
     };
-    
   }
   
   /**
    * Step 3 - Axios sends GET request 
    * Step 4 - Response recieved and logged in console - Line 23
+   * 
    */
   async getJoke() {
     try {
       const response = await axios.get('http://api.icndb.com/jokes/random')
-      console.log(response)
+      this.setState({joke: response.data.value})
     } catch (error) {
       console.error(error)
     }
@@ -31,13 +31,16 @@ class App extends Component {
    * Step 2 - getJoke function is called - Line 40
    */
   render() {
+    const { joke } = this.state
+    console.log(joke)
+    
     return(
-      <div className="container">
+      <div className='container'>
         <h1>Chuck Norris Jokes</h1>
-        <h3>Here's one..</h3>
-        <button type="button" 
-          className="btn btn-primary"
-          onClick={this.getJoke}> 
+        <h3>Here's one..{joke && joke.id ? <b>{joke.joke}</b>: ''}</h3>
+        <button type='button' 
+          className='random-button'
+          onClick={() => this.getJoke()}> 
           Random Chuck Jokes!
         </button>
       </div>
