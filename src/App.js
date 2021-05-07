fa2cc61c4b32a7aa0aa913284bae6879b8fb2c57
Adam Horscraft <adam.horscraft@foodhub.co.uk>
2021-05-07 10:18:27 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * Step 5 - The state has been set on lines 10 & 11 and the DOM is updated
+ */
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newJoke: '',
+    };
+    
+  }
+  
+  /**
+   * Step 3 - Axios sends GET request 
+   * Step 4 - Response recieved and logged in console - Line 23
+   */
+  async getJoke() {
+    try {
+      const response = await axios.get('http://api.icndb.com/jokes/random')
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  /**
+   * Step 1 - User Clicks Button - Line 40
+   * Step 2 - getJoke function is called - Line 40
+   */
+  render() {
+    return(
+      <div className="container">
+        <h1>Chuck Norris Jokes</h1>
+        <h3>Here's one..</h3>
+        <button type="button" 
+          className="btn btn-primary"
+          onClick={this.getJoke}> 
+          Random Chuck Jokes!
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
